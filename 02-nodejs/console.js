c0fc87ca87210e25f2e673811092c9231bcd123a
thien ban BUI <thienban91@hotmail.fr>
2018-01-04 17:32:13 +0100

    menu

--- 02-nodejs/console.js
+++ 02-nodejs/console.js
@@ -5,3 +5,9 @@ console.log(service.filtrerTopPresentateurs())
 console.log(service.listerTousLesPresentateurs())
 console.log(service.listerToutesLesSessions())
 console.log(service.trouverUneSession())
+
+console.log("*** Application Conférence ***\n"
+    + "1. Liste de tous les présentateurs 1\n"
+    + "2. Top présentateurs 2\n"
+    + "3. Liste des sessions 3\n"
+    + "4. Détail d'une session")
